5d5f5de9bfe1041f37564e0e2f1742052a93c0e7
Haik <haik.asatran@pandatech.it>
2023-11-26 14:12:14 +0400

    better implementation

---
+++ PandaVaultClient/ApplicationBuilderExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PandaVaultClient;
+
+public static class ApplicationBuilderExtension
+{
+    public static void RunPandaVaultClient(this WebApplication app)
+    {
+        var service = app.Services.GetRequiredService<PandaVault>();
+        service.SetConfigurationsAsync().GetAwaiter().GetResult();
+    }
+}
---
+++ PandaVaultClient/PandaVaultApi.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PandaVaultClient;
+
+public static class PandaVaultApi
+{
+    public static void MapPandaVaultApi(this WebApplication app)
+    {
+        app.MapGet("/configurations",
+                (PandaVault vault, [FromHeader] string secret) => vault.GetAllConfigurations(secret))
+            .WithTags("Above Board");
+
+        app.MapPatch("/configurations",
+                async (PandaVault vault, [FromHeader] string secret) => await vault.RefreshConfigurationsAsync(secret))
+            .WithTags("Above Board");
+    }
+}
